64efa3210fcfab944afca1fcf2e84fcd0784841c
rishabh <42717036+rishabh-khemka@users.noreply.github.com>
2018-11-04 18:12:36 +0530

    Update cases.js

--- routes/cases.js
+++ routes/cases.js
@@ -4,12 +4,11 @@ var express= require("express"),
 
 
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'rishabh',
-  password : 'arkay',
-  database : 'projectdb',
-   socketPath: "/var/run/mysqld/mysqld.sock"
- });
+    	host     : process.env.MYSQL_ADDON_HOST,
+    	database : process.env.MYSQL_ADDON_DB,
+    	user     : process.env.MYSQL_ADDON_USER,
+    	password : process.env.MYSQL_ADDON_PASSWORD
+  });
  
  
  router.post("/cases/addnew", function(req,res){
