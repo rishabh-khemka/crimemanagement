8be7df957b8f364059e15e464ed4ac8005299649
rishabh <42717036+rishabh-khemka@users.noreply.github.com>
2018-11-05 15:33:38 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,7 @@ var express= require("express"),
     router=express.Router();
     
 
-var mysql      = require('mysql');
-  var connection = mysql.createConnection({
-    	host     : process.env.MYSQL_ADDON_HOST,
-    	database : process.env.MYSQL_ADDON_DB,
-    	user     : process.env.MYSQL_ADDON_USER,
-    	password : process.env.MYSQL_ADDON_PASSWORD
-  });
+ var connection = require("./database");
  
  
  
